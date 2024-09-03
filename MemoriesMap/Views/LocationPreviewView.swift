2f626101d0ea3c2630640a74feb9c9e19a307ca1
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2024-09-04 01:49:29 +0530

    feat: location preview ui and next button integration

---
+++ MemoriesMap/Views/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  MemoriesMap
+//
+//  Created by Nadeem Noushad on 04/09/24.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
