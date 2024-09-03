986dcd6d3e1f6d3838a0879876a0f87f9f1eb376
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2024-09-03 19:38:47 +0530

    Initial Commit

---
+++ MemoriesMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoriesMap
+//
+//  Created by Nadeem Noushad on 03/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoriesMap/MemoriesMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesMapApp.swift
+//  MemoriesMap
+//
+//  Created by Nadeem Noushad on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
