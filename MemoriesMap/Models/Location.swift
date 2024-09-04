c85d3917e21cd5e89aabe56afb76935a71ff155c
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2024-09-04 16:04:37 +0530

    refactor: made imagesName array optional

--- MemoriesMap/Models/Location.swift
+++ MemoriesMap/Models/Location.swift
@@ -13,7 +13,7 @@ struct Location: Identifiable, Equatable {
     let cityName: String
     let coordinates: CLLocationCoordinate2D
     let description: String
-    let imageNames: [String]
+    let imageNames: [String]?
     let link: String
     
     var id: String {
